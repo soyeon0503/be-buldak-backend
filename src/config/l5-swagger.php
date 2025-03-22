7d6f57dcfa8d90f340f6466990797f16a9e20351
soyeon <bless0053@gmail.com>
2025-03-22 19:59:49 +0900

    feature:swgger추가

---
+++ src/config/l5-swagger.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'paths' => [
+        'docs' => storage_path('api-docs'),
+    ],
+    'generate_always' => true,
+    'swagger_version' => '3.0',
+];
--- src/routes/web.php
+++ src/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// swagger
+Route::get('/api/docs.json', function () {
+    return response()->file(storage_path('api-docs/swagger.json'));
+});
