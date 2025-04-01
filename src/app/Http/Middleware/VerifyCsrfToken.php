b5b9745d4338d6e50bc40ec4499822539cdf6b64
soyeon <bless0053@gmail.com>
2025-04-01 23:14:38 +0900

    VerifyCsrfToken추가

---
+++ src/app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+
+class VerifyCsrfToken extends Middleware
+{
+    /**
+     * CSRF 검증을 제외할 URL 리스트
+     *
+     * @var array<int, string>
+     */
+    protected $except = [
+        'api/auth/csrf-token',
+        'api/login',
+        'api/logout',
+    ];
+    
+}
+
