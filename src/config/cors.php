fb4c16999f29515c9dc2464fd78fe3e154feb6a3
soyeon <bless0053@gmail.com>
2025-07-15 15:50:27 +0900

    fix:cors에러수정

--- src/config/cors.php
+++ src/config/cors.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:8080', "http://localhost:8081"], // Next.js 로컬 개발 환경
+    'allowed_origins' => ['*'], // Next.js 로컬 개발 환경
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 ];
